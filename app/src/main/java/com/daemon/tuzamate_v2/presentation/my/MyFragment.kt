26ac99b8e229042764727aaa1da8f5db0c8d7cd6
Daemon Jung <jjjssswww1006@gmail.com>
2025-07-31 20:31:35 +0900

    feat: Add MyFragment and its layout
    This commit introduces the `MyFragment` and its corresponding layout file `fragment_my.xml`.
    
    **MyFragment.kt:**
    - Initializes with view binding.
    - Handles navigation controller setup.
    - Ensures the bottom navigation bar is visible when this fragment is displayed.
    
    **fragment_my.xml:**
    - Defines the UI for the "My" screen, including:
        - App bar with notification and settings icons.
        - Profile section with user image and name.
        - Credit status display.
        - Sections for "App Info" and "Account Management" with respective options:
            - Terms of Use
            - Privacy Policy
            - Inquiry
            - Version Info
            - Logout
    - Uses ConstraintLayout for structuring the elements.
    - Applies predefined styles and colors.

---
+++ app/src/main/java/com/daemon/tuzamate_v2/presentation/my/MyFragment.kt
@@ -0,0 +1,43 @@
+package com.daemon.tuzamate_v2.presentation.my
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.daemon.tuzamate_v2.MainActivity
+import com.daemon.tuzamate_v2.databinding.FragmentMyBinding
+
+class MyFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    private var _binding: FragmentMyBinding? = null
+    private val binding: FragmentMyBinding
+        get() = requireNotNull(_binding){"FragmentMyBinding -> null"}
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+        _binding = FragmentMyBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = view.findNavController()
+
+        (requireActivity() as MainActivity).hideBottomNavigation(false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
